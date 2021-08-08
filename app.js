4722212f679f1e659706a342f00b7a483bf40c9c
isedgar <85322271+isedgar@users.noreply.github.com>
2021-08-08 14:45:40 -0400

    update n2

--- app.js
+++ app.js
@@ -1,145 +1,32 @@
-var inter={
-    linear: function(v, l){
-        var n = v.length,
-            step = n / l,
-            result = [],
-            x, i, j, w1, w2,
-            lim = n - 1 - 1.19e-07;
+var open = document.getElementById('open'),
+    subwrapper = document.getElementById('subwrapper'),
+    input = document.getElementById('input'),
+    output = document.getElementById('output'),
+    input_size = document.getElementById('input_size'),
+    output_size = document.getElementById('output_size'),
+    type = document.getElementById('type'),
+    factor = document.getElementById('factor'),
+    loader = document.getElementById('loader');
 
-        for(var k=0; k < l; ++k){
-            x = (k + 0.5)*step - 0.5;
+var sw = window.innerWidth,
+    sh = window.innerHeight,
+    input_top = 0, input_left,
+    output_top = 0, output_left;
 
-            x = x < 0 ? 0 : x;
-            x = x > lim ? lim : x;
+subwrapper.style.height = (sh - 50) + 'px';
 
-            i = Math.floor(x);
-            j = i + 1;
+if(sh > sw){
+    subwrapper.style.flexDirection = 'column';
 
-            w1 = j - x;
-            w2 = 1 - w1;
+    input.style.width = '100%';
+    output.style.width = '100%';
 
-            result.push(v[i]*w1 + v[j]*w2);
-        }
-
-        return result;
-    },
-
-    cubic: function(v,l){
-        var n = v.length,
-            step = n / l,
-            result = [],
-            x, i, j, t, y0, y1, m0, m1, a, b, c, d,
-            u, extra_pt;
-
-        extra_pt = v[0]*2 - v[1];
-
-        // u = [extra_pt*2 - v[0], extra_pt];
-        u = [v[0], v[0]];
-
-        for(var k=0; k < n; ++k) u.push(v[k]);
-
-        extra_pt = v[n-1]*2 - v[n-2];
-
-        // u.push(extra_pt, extra_pt*2 - v[n-1]);
-        u.push(v[n-1], v[n-1]);
-
-
-        for(var k=0; k < l; ++k){
-            x = (k + 0.5)*step + 1.5;
-
-            i = Math.floor(x);
-            j = i + 1;
-
-            t = 1 - (j - x);
-
-            y0 = u[i];
-            y1 = u[j]
-            m0 = (u[j] - u[i-1]) / 2;
-            m1 = (u[j+1] - u[i]) / 2
-
-            a = 2*y0 - 2*y1 + m0 + m1;
-            b = -3*y0 + 3*y1 - 2*m0 - m1;
-            c = m0;
-            d = y0;
-
-            result.push( a*Math.pow(t,3) + b*Math.pow(t,2) + c*t + d );
-        }
-
-        return result;
-        
-    },
-
-    bilinear: function(m, nh, nw){
-        var h = m.length,
-            result = [],
-            rows = [],
-            col, new_col;
-
-        result = this.zeros2d(nh, nw);
-
-        for(var k=0; k < h; ++k){
-            rows.push( this.linear( m[k], nw ) );
-        }
-
-        for(var k=0; k < nw; ++k){
-            col = [];
-
-            for(var j=0; j < h; ++j) col.push(rows[j][k]);
-
-            new_col = this.linear(col, nh);
-
-            for(var j=0; j < nh; ++j){
-                result[j][k] = new_col[j];
-            }
-        }
-
-        return result;
-    },
-
-    bicubic: function(m, nh, nw){
-        var h = m.length,
-            result = [],
-            rows = [],
-            col, new_col;
-
-        result = this.zeros2d(nh, nw);
-
-        for(var k=0; k < h; ++k){
-            rows.push( this.cubic( m[k], nw ) );
-        }
-
-        for(var k=0; k < nw; ++k){
-            col = [];
-
-            for(var j=0; j < h; ++j) col.push(rows[j][k]);
-
-            new_col = this.cubic(col, nh);
-
-            for(var j=0; j < nh; ++j){
-                result[j][k] = new_col[j];
-            }
-        }
-
-        return result;
-    },
-
-    zeros2d: function(h,w){
-        var result = [];
-
-        for(var k=0; k < h; ++k){
-            result.push([]);
-            row = result[k];
-
-            for(var j=0; j < w; ++j){
-                row.push(0);
-            }
-        }
-
-        return result;
-    }
+    input.style.height = '50%';
+    output.style.height = '50%';
 }
 
-var open = document.getElementById('open');
+var M = null;
+
 
 open.addEventListener('change', function(){
     var file = this.files[0],
@@ -151,37 +38,15 @@ open.addEventListener('change', function(){
         var img = new Image();
 
         img.addEventListener('load', function(){
-            var matrix = reshape_gray(this),
-                w = matrix[0].length, h=matrix.length,
-                canvas_input = document.createElement('canvas'),
-                canvas = document.createElement('canvas'),
-                ctx = canvas.getContext('2d'),
-                ctx_input = canvas_input.getContext('2d'),
-                nw, nh, result, start, end;
+            M = reshape_gray(this);
 
-            nw = parseInt(w*parseFloat(factor.value)); nh =parseInt(h*parseFloat(factor.value));
+            var canvas_input = document.createElement('canvas'),
+                ctx_input = canvas_input.getContext('2d'),
+                w = M[0].length,
+                h = M.length;
 
             canvas_input.width = w;
             canvas_input.height = h;
-
-            canvas.width = nw;
-            canvas.height = nh;
-
-            start = Date.now();
-
-            if(type.value == 'linear') result = inter.bilinear(matrix, nh, nw);
-            else if(type.value == 'cubic') result = inter.bicubic(matrix, nh, nw);
-
-            // result = inter.bicubic(matrix, nh, nw);
-
-            ctx.putImageData(toImageData(result), 0, 0);
-
-            ctx_input.drawImage(this, 0, 0);
-
-            end = Date.now();
-
-            console.log(end - start, 'ms');
-
 
             canvas_input.style.position = 'absolute';
             canvas_input.style.top = 0+'';
@@ -194,28 +59,100 @@ open.addEventListener('change', function(){
                 canvas_input.style.left = (input.clientWidth/2 - w/2) + 'px';
             }
 
+            if(h < input.clientHeight && w < input.clientWidth){
+                input.style.cursor = 'default';
+            }
+            else input.style.cursor = 'grab';
+
+            if(input.lastChild) input.removeChild(input.lastChild);
+            //=======================================================================
+            //=======================================================================
+
+            ctx_input.drawImage(this, 0, 0);
+
             input.appendChild(canvas_input);
-            //=================================================================
 
-            canvas.style.position = 'absolute';
-            canvas.style.top = 0+'';
-            canvas.style.left = 0+'';
+            input_size.left = input.getBoundingClientRect().x + 'px';
+            input_size.top = input.getBoundingClientRect().y + 'px';
+            input_size.style.opacity = 1;
+            input_size.innerHTML = w + ' x ' + h + ' px';
 
-            if(nh < output.clientHeight){
-                canvas.style.top = (output.clientHeight/2 - nh/2) + 'px';
-            }
-            if(nw < output.clientWidth){
-                canvas.style.left = (output.clientWidth/2 - nw/2) + 'px';
-            }
-
-            if(output.lastChild) output.removeChild(output.lastChild);
-
-            output.appendChild(canvas);
+            update_result();
         });
         
         img.src = this.result;
     });
 });
+
+function update_result(){
+    var matrix = M;
+
+    
+    if(matrix){
+    //=======================================================
+
+    loader.style.opacity = 1;
+
+    var w = matrix[0].length, h = matrix.length;
+
+    nw = parseInt(Math.round(w*parseFloat(factor.value)));
+    nh = parseInt(Math.round(h*parseFloat(factor.value)));
+
+    var worker = new Worker('worker.js');
+
+    worker.postMessage({type: type.value, size: {w: nw, h: nh}, matrix: matrix});
+
+    worker.addEventListener('message', function(e){
+        loader.style.opacity = 0;
+
+        var result = e.data.matrix,
+            time = e.data.time;
+
+        console.log(time, 'ms');
+
+        var canvas_output = document.createElement('canvas'),
+            ctx_output = canvas_output.getContext('2d'),
+            nh, nw;
+
+        nh = result.length;
+        nw = result[0].length
+    
+        canvas_output.width = nw;
+        canvas_output.height = nh;
+    
+        canvas_output.style.position = 'absolute';
+        canvas_output.style.top = 0+'';
+        canvas_output.style.left = 0+'';
+    
+        if(nh < output.clientHeight){
+            canvas_output.style.top = (output.clientHeight/2 - nh/2) + 'px';
+        }
+        if(nw < output.clientWidth){
+            canvas_output.style.left = (output.clientWidth/2 - nw/2) + 'px';
+        }
+    
+        if(nh < output.clientHeight && nw < output.clientWidth){
+            output.style.cursor = 'default';
+        }
+        else output.style.cursor = 'grab';
+    
+        if(output.lastChild) output.removeChild(output.lastChild);
+    
+        ctx_output.putImageData(toImageData(result), 0, 0);
+    
+        output.appendChild(canvas_output);
+
+        output_size.left = output.getBoundingClientRect().x + 'px';
+        output_size.top = output.getBoundingClientRect().y + 'px';
+        output_size.style.opacity = 1;
+        output_size.innerHTML = nw + ' x ' + nh + ' px';
+    });
+        
+
+
+    //=======================================================
+    }
+}
 
 function reshape_gray(img){
     var w = img.naturalWidth,
@@ -328,37 +265,20 @@ function toImageData(m){
     return imgdata;
 }
 
-var m = [
-    [3, 5],
-    [2, 9]
-];
-
-var v = [3,5,2,9];
-
-// console.log(inter.linear(v, 10));
-
-// console.log(inter.cubic(v, 10));
-
-// console.log(inter.bilinear(m, 5, 5));
-
-// console.log(inter.bicubic(m, 5, 5));
-
-
-var input = document.getElementById('input'),
-    output = document.getElementById('output'),
-    type = document.getElementById('type'),
-    factor = document.getElementById('factor');
+var default_factor = 3;
 
 type.addEventListener('change', function(){
-
+    update_result();
 });
 
 factor.addEventListener('change', function(){
     if(this.value == ''){
-        document.getElementById('open-btn').style.pointerEvents = 'none'
+        this.value = default_factor;
     }
     else{
-        document.getElementById('open-btn').style.pointerEvents = 'auto';
+        default_factor = this.value;
+
+        update_result();
     }
 });
 
@@ -368,3 +288,83 @@ if(factor.value == ''){
 else{
     document.getElementById('open-btn').style.pointerEvents = 'auto';
 }
+
+['resize','fullscreenchange'].forEach(e=>{
+//============================================
+
+window.addEventListener(e, function(){
+    sw = window.innerWidth,
+    sh = window.innerHeight;
+
+    subwrapper.style.height = (sh - 50) + 'px';
+
+    if(sh > sw){
+        subwrapper.style.flexDirection = 'column';
+
+        input.style.width = '100%';
+        output.style.width = '100%';
+    
+        input.style.height = '50%';
+        output.style.height = '50%';
+    }
+
+    else {
+        subwrapper.style.flexDirection = 'row';
+
+        input.style.width = '50%';
+        output.style.width = '50%';
+    
+        input.style.height = '100%';
+        output.style.height = '100%';
+    }
+
+    var canvas_input = input.getElementsByTagName('canvas')[0],
+        canvas_output = output.getElementsByTagName('canvas')[0];
+
+    if(canvas_input){
+        canvas_input.style.top = 0;
+        canvas_input.style.left = 0;
+
+        if(canvas_input.height < input.clientHeight){
+            canvas_input.style.top = (input.clientHeight/2 - canvas_input.height/2) + 'px';
+        }
+        if(canvas_input.width < input.clientWidth){
+            canvas_input.style.left = (input.clientWidth/2 - canvas_input.width/2) + 'px';
+        }
+
+        if(canvas_input.height < input.clientHeight && canvas_input.width < input.clientWidth){
+            input.style.cursor = 'default';
+        }
+        else input.style.cursor = 'grab';
+
+        input_size.left = input.getBoundingClientRect().x + 'px';
+        input_size.top = input.getBoundingClientRect().y + 'px';
+        input_size.style.opacity = 1;
+        input_size.innerHTML = canvas_input.width + ' x ' + canvas_input.height + ' px';
+    }
+
+    if(canvas_output){
+        canvas_output.style.top = 0;
+        canvas_output.style.left = 0;
+
+        if(canvas_output.height < output.clientHeight){
+            canvas_output.style.top = (output.clientHeight/2 - canvas_output.height/2) + 'px';
+        }
+        if(canvas_output.width < output.clientWidth){
+            canvas_output.style.left = (output.clientWidth/2 - canvas_output.width/2) + 'px';
+        }
+
+        if(canvas_output.height < output.clientHeight && canvas_output.width < output.clientWidth){
+            output.style.cursor = 'default';
+        }
+        else output.style.cursor = 'grab';
+
+        output_size.left = output.getBoundingClientRect().x + 'px';
+        output_size.top = output.getBoundingClientRect().y + 'px';
+        output_size.style.opacity = 1;
+        output_size.innerHTML = canvas_output.width + ' x ' + canvas_output.height + ' px';
+    }
+});
+
+//============================================
+});
---
+++ worker.js
@@ -0,0 +1,216 @@
+var inter={
+    linear: function(v, l){
+        var n = v.length,
+            step = n / l,
+            result = [],
+            x, i, j, w1, w2,
+            lim = n - 1 - 1.19e-07;
+
+        for(var k=0; k < l; ++k){
+            x = (k + 0.5)*step - 0.5;
+
+            x = x < 0 ? 0 : x;
+            x = x > lim ? lim : x;
+
+            i = Math.floor(x);
+            j = i + 1;
+
+            w1 = j - x;
+            w2 = 1 - w1;
+
+            result.push(v[i]*w1 + v[j]*w2);
+        }
+
+        return result;
+    },
+
+    cubic: function(v,l){
+        var n = v.length,
+            step = n / l,
+            result = [],
+            x, i, j, t, y0, y1, m0, m1, a, b, c, d, u;
+
+
+        u = [v[0], v[0]];
+
+        for(var k=0; k < n; ++k) u.push(v[k]);
+
+        u.push(v[n-1], v[n-1]);
+
+
+        for(var k=0; k < l; ++k){
+            x = (k + 0.5)*step + 1.5;
+
+            i = Math.floor(x);
+            j = i + 1;
+
+            t = 1 - (j - x);
+
+            y0 = u[i];
+            y1 = u[j];
+            m0 = (u[j] - u[i-1]) / 2;
+            m1 = (u[j+1] - u[i]) / 2;
+
+            a = 2*y0 - 2*y1 + m0 + m1;
+            b = -3*y0 + 3*y1 - 2*m0 - m1;
+            c = m0;
+            d = y0;
+
+            result.push( a*Math.pow(t,3) + b*Math.pow(t,2) + c*t + d );
+        }
+
+        return result;
+        
+    },
+
+    lanczos: function(v, l, a){
+        a = a == undefined ? 4 : a; // determines the size of the kernel: a * 2 - 1
+
+        var n = v.length,
+            step = n / l,
+            result = [],
+            j, x, xf, y; // xf: x_floor
+
+        for(var dx = 0; dx < l; ++dx){
+            x = (dx + 0.5)*step - 0.5;
+            xf = Math.floor(x);
+
+            y = 0;
+
+            for(var i = xf-a+1; i < xf+a+1; ++i){
+                j = i;
+
+                if(i < 0) j = 0;
+                else if(i > n-1) j = n-1;
+
+                y += v[j] * this.lanczos_kernel(x - i, a);
+            }
+
+            result.push(y);
+        }
+
+        return result;
+    },
+
+    lanczos_kernel: function(x, a){
+        if(x == 0) return 1;
+        else if(x >= -a && x < a){
+            return (a*Math.sin(Math.PI*x)*Math.sin(Math.PI*x/a)) / (Math.pow(Math.PI,2)*Math.pow(x,2));
+        }
+        else return 0;
+    },
+
+    bilinear: function(m, nh, nw){
+        var h = m.length,
+            result = [],
+            rows = [],
+            col, new_col;
+
+        result = this.zeros2d(nh, nw);
+
+        for(var k=0; k < h; ++k){
+            rows.push( this.linear( m[k], nw ) );
+        }
+
+        for(var k=0; k < nw; ++k){
+            col = [];
+
+            for(var j=0; j < h; ++j) col.push(rows[j][k]);
+
+            new_col = this.linear(col, nh);
+
+            for(var j=0; j < nh; ++j){
+                result[j][k] = new_col[j];
+            }
+        }
+
+        return result;
+    },
+
+    bicubic: function(m, nh, nw){
+        var h = m.length,
+            result = [],
+            rows = [],
+            col, new_col;
+
+        result = this.zeros2d(nh, nw);
+
+        for(var k=0; k < h; ++k){
+            rows.push( this.cubic( m[k], nw ) );
+        }
+
+        for(var k=0; k < nw; ++k){
+            col = [];
+
+            for(var j=0; j < h; ++j) col.push(rows[j][k]);
+
+            new_col = this.cubic(col, nh);
+
+            for(var j=0; j < nh; ++j){
+                result[j][k] = new_col[j];
+            }
+        }
+
+        return result;
+    },
+
+    bilanczos: function(m, nh, nw, a){
+        a = a == undefined ? 4 : a; // determines the size of the kernel: a * 2 - 1
+
+        var h = m.length,
+            result = [],
+            rows = [],
+            col, new_col;
+
+        result = this.zeros2d(nh, nw);
+
+        for(var k=0; k < h; ++k){
+            rows.push( this.lanczos( m[k], nw, a ) );
+        }
+
+        for(var k=0; k < nw; ++k){
+            col = [];
+
+            for(var j=0; j < h; ++j) col.push(rows[j][k]);
+
+            new_col = this.lanczos(col, nh, a);
+
+            for(var j=0; j < nh; ++j){
+                result[j][k] = new_col[j];
+            }
+        }
+
+        return result;
+    },
+
+    zeros2d: function(h,w){
+        var result = [];
+
+        for(var k=0; k < h; ++k){
+            result.push([]);
+            row = result[k];
+
+            for(var j=0; j < w; ++j){
+                row.push(0);
+            }
+        }
+
+        return result;
+    },
+
+    matrix: null
+}
+
+self.addEventListener('message', function(e){
+    var data = e.data, result, start_time, end_time;
+
+    start_time = Date.now();
+        
+    if(data.type == 'linear') result = inter.bilinear(data.matrix, data.size.h, data.size.w);
+    else if(data.type == 'cubic') result = inter.bicubic(data.matrix, data.size.h, data.size.w);
+    else if(data.type == 'lanczos') result = inter.bilanczos(data.matrix, data.size.h, data.size.w);
+
+    end_time = Date.now();
+
+    self.postMessage({matrix: result, time: end_time - start_time});
+});
